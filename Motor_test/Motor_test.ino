e4ab52815537aa70d3fd2996b7e9eda467c94cda
pedrobarroseng <pedrovbarros17@gmail.com>
2026-04-13 21:47:54 -0300

    Versão final

--- Motor_test/Motor_test.ino
+++ Motor_test/Motor_test.ino
@@ -3,102 +3,162 @@
 #include <Adafruit_PCD8544.h>
 #include <Servo.h>
 
+//Variaveis para controlar o motor
 Servo motor;
 int potenciometro = A0;
 int pinoEsc = 8;
 
-unsigned long leituraInicial = 0;
+//Variaveis para a camada de segurança
+int long leituraInicial = 0;
 bool sistemaTravado = false;
 bool motorArmado = false;
 
-// Configuração LCD: CLK, DIN, DC, CE, RST
+//Variavel para a escolha dos ESC (Unidirecional ou bidirecional)
+int ESC = 22;
+
+// Configuração: CLK, DIN, DC, CE (CR), RST
 Adafruit_PCD8544 display = Adafruit_PCD8544(13, 12, 11, 10, 9);
 
 void setup() {
+
   Serial.begin(9600);
-  
+
+  //Parte da tela inicialização da tela
   display.begin();
-  display.setContrast(50); // Baixei um pouco para estabilizar o consumo
-  display.clearDisplay();
-  display.setTextSize(1);
+  // Ajuste de contraste
+  display.setContrast(55);
+  display.clearDisplay(); 
+  display.setTextSize(1); 
   display.setTextColor(BLACK);
-  
   display.setCursor(10,20);
-  display.println("MODO TESTE");
-  display.display();
 
+  display.println("MOTOR TEST");
+
+  display.display(); 
+
+  //Parte do controle dos motores com potenciometro
   pinMode(potenciometro, INPUT);
-  
-  motor.attach(pinoEsc);
-  motor.writeMicroseconds(1000); // Sinal de segurança inicial
 
+  //Parte que decidimos via chave gangorra
+  pinMode(ESC, INPUT_PULLUP);
+  
+  //Envio do sinal para o motor
+  motor.attach(pinoEsc);
+  motor.writeMicroseconds(1000);
+
+  //Variavel que faz a leitura incial do potenciometro para os calculos da camada de segurança
   leituraInicial = analogRead(potenciometro);
   delay(2000);
 }
 
 void loop() {
-  int leitura_potenciometro = analogRead(potenciometro);
-  int sinalFinal;
-  int porcentagem;
 
-  // --- TRAVA 1: SEGURANÇA DE CONEXÃO (FAILSAFE) ---
-  unsigned long leituraAtual = analogRead(potenciometro);
-  if (abs(leituraAtual - leituraInicial) >= 1000) {
+  int leitura_potenciometro;
+  int sinalFinal;
+  int porcentagem;  
+
+  leitura_potenciometro = analogRead(potenciometro);
+
+  //leitura do potenciometro para o calculo da camada de segurança
+  int long leituraAtual = analogRead(potenciometro);
+  if (abs(leituraAtual - leituraInicial) >= 512){
+    Serial.print(leituraAtual);
+    Serial.print("-");
+    Serial.println(leituraInicial);
+    Serial.print("=");
+    Serial.println(leituraAtual - leituraInicial);
     sistemaTravado = true;
+    
+  }
+  if (sistemaTravado == true){
+    motor.writeMicroseconds(1000);
+
+    display.setContrast(55);
+    display.clearDisplay(); 
+    display.setTextSize(1); 
+    display.setTextColor(BLACK);
+    display.setCursor(0,20);
+    display.print("connection error");
+    display.display();
   }
 
-  if (sistemaTravado) {
-    motor.writeMicroseconds(1000);
-    display.clearDisplay();
-    display.setCursor(0, 20);
-    display.print("ERRO CONEXAO");
-    display.display();
-  } 
-  else {
-    // --- TRAVA 2: ARMAÇÃO (ARMING) ---
-    if (!motorArmado && leitura_potenciometro < 80) {
+
+  else{
+
+    if (leitura_potenciometro < 50){
       motorArmado = true;
     }
 
-    if (motorArmado) {
-      // Mapeamento fixo para teste (Unidirecional)
-      sinalFinal = map(leitura_potenciometro, 0, 1023, 1000, 1900);
-      porcentagem = map(leitura_potenciometro, 0, 1023, 0, 100);
-
+    if (motorArmado == true){
+       
+       display.clearDisplay();
+       display.setContrast(55);
+       display.clearDisplay(); 
+       display.setTextSize(1); 
+       display.setTextColor(BLACK);
       
+      if (digitalRead(ESC) == HIGH){ 
+       
+        display.setCursor(0,0);
+        display.print("ESC Uni-dir");
+        sinalFinal = map(leitura_potenciometro, 0, 1023, 1000, 1900);
+        porcentagem = map(leitura_potenciometro, 0, 1023, 0, 100);
+        }
+      
+      else{
+        
+        display.setCursor(0,0);
+        display.print("ESC bi-dir");
+        sinalFinal = map(leitura_potenciometro, 0, 1023, 1000, 2000);
+        porcentagem = map(leitura_potenciometro, 0, 1023, -100, 100);
+        }
+        
+     
       Serial.print(sinalFinal);
       Serial.print("|");
       Serial.println(leitura_potenciometro);
-      
+
       motor.writeMicroseconds(sinalFinal);
 
-      display.clearDisplay();
-      display.setCursor(0, 0);
-      display.print("MOTOR ATIVO");
-      
-      display.setCursor(0, 15);
-      display.print("Pot: "); display.print(porcentagem); display.print("%");
-      
-      display.setCursor(0, 30);
-      display.print("PWM: "); display.print(sinalFinal);
-      
-      display.setCursor(0, 40);
-      display.print("ADC: "); display.print(leitura_potenciometro);
-      
-      display.display();
-    } 
-    else {
-      // Enquanto não arma
-      motor.writeMicroseconds(1000);
-      display.clearDisplay();
-      display.setCursor(0, 15);
-      display.print("COLOQUE NO 0");
+      //Parte que plota os sinais na tela
+      display.setCursor(0, 10);
+      display.print("Pot: ");
+      display.print(porcentagem);
+      display.println("%");
+      display.print("--------------");
+  
       display.setCursor(0, 25);
-      display.print("PARA ARMAR");
+      display.print("PWM (ms): ");
+      display.println(sinalFinal);
+      display.print("--------------");
+
+      display.setCursor(0, 40);
+      display.print("ADC:");
+      display.print(leitura_potenciometro);
+
+      display.display();    
+      
+  }
+    else{
+      //Isso vai enviar um sinal para o motor estabilizar no zero caso não esteja
+       if(digitalRead(ESC) == HIGH){
+        motor.writeMicroseconds(1000);
+    }
+      else{
+        motor.writeMicroseconds(1500);
+    }
+      //Aviso para colocar o poteniometro no 0, vulgo o ponto de referencia inicial
+      display.setContrast(55);
+      display.clearDisplay(); 
+      display.setTextSize(1); 
+      display.setTextColor(BLACK);
+      display.setCursor(0,20);
+      display.print("Coloque no 0");
+      display.println("para armar");
       display.display();
     }
-    leituraInicial = leituraAtual;
-
   }
-  delay(50); // Pequeno delay para ajudar na estabilidade do LCD
+  leituraInicial = leituraAtual;
+  delay(10)
 }
+  
